0fb024336c7cf436b24c206aca199fbf6904f374
Mohammed Salam <mohammed.salam912@myhunter.cuny.edu>
2019-09-27 12:56:08 -0400

    PHASE TWO AVENGERS ASSEMBLE

---
+++ DeckOfCards/Models/CardDrawnResponse.cs
@@ -0,0 +1,19 @@
+﻿using DeckOfCards.Data;
+using System.Collections.Generic;
+
+namespace DeckOfCards.Models
+{
+    public class CardDrawnResponse
+    {
+        public CardDrawnResponse()
+        {
+            Removed = new List<CardInfo>();
+        }
+
+        public string DeckId { get; set; }
+
+        public int Remaining { get; set; }
+
+        public List<CardInfo> Removed { get; set; }
+    }
+}
